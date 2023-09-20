a8dab1eacc50115dff6b465952fe20ce37726299
lucas <lucasgonzalez5500@gmail.com>
2023-09-20 12:44:04 -0300

    .

--- src/components/blog/Blog.vue
+++ src/components/blog/Blog.vue
@@ -57,5 +57,9 @@ export default {
 
 	.content
 		img 
-			max-width: 600px
+			@media screen and (max-width: 768px)
+				max-width: 100%
+			@media screen and (min-width: 768px)
+				display: flex
+				max-width: 700px
 </style>
